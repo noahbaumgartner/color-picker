a447b616d0c205082233c426febd1a1c03f70b0b
Noah <baumgartner.noah@outlook.com>
2021-06-14 21:46:55 +0200

    changed Picker

--- src/app/picker/picker.component.ts
+++ src/app/picker/picker.component.ts
@@ -6,14 +6,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./picker.component.scss']
 })
 export class PickerComponent implements OnInit {
+  hexCode = '';
+  sliderColorClass = '';
 
-  constructor() { }
+  red = 0;
+  green = 0;
+  blue = 0;
 
-  ngOnInit(): void {
+  constructor() {
+    this.red = this.getRandomNumber();
+    this.green = this.getRandomNumber();
+    this.blue = this.getRandomNumber();
+
+    this.setHexCode();
   }
 
-  changeColor() {
-    console.log();
+  ngOnInit(): void { }
+
+  changeColor(red: string, green: string, blue: string) {
+    this.red = parseInt(red);
+    this.green = parseInt(green);
+    this.blue = parseInt(blue);
+
+    this.setHexCode();
   }
 
+  convertRGBtoHex(n: Number) {
+    var hex = n.toString(16);
+    while (hex.length < 2) {hex = "0" + hex; }
+    return hex;
+  }
+
+  getRandomNumber() {
+    return Math.floor(Math.random() * 255);
+  }
+  
+  setHexCode() {
+    var redH = this.convertRGBtoHex(this.red);
+    var greenH = this.convertRGBtoHex(this.green);
+    var blueH = this.convertRGBtoHex(this.blue);
+
+    if(this.red + this.green + this.blue > 300) {
+      this.sliderColorClass = 'slider_black';
+    } else {
+      this.sliderColorClass = 'slider_white';
+    }
+
+    this.hexCode = '#' + redH + greenH + blueH;
+  }
 }
